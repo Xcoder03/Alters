24acece207683dc4cc3d0cc8970b9ece4ab0fb7a
Xcoder <chindaebube03@gmail.com>
2022-12-21 23:07:05 +0100

    Updated address controller and implemented methods from address service

--- src/main/java/com/springclass/student/controller/AddressController.java
+++ src/main/java/com/springclass/student/controller/AddressController.java
@@ -1,6 +1,30 @@
 package com.springclass.student.controller;
 
+import com.springclass.student.model.Address;
+import com.springclass.student.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/vi/address")
 public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping()
+    public List<Address> getallstudent(){
+        return  addressService.getAllddressDetails();
+    }
+
+    @PutMapping("/{id}")
+    public String  updateAddresst(@PathVariable(name="id")Long id, @Valid  @RequestBody Address address){
+        return addressService.updateaddress(id,address);
+
+    }
+
 
 }
 
